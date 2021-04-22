7261d8bcfa1eecca8eda66cc903bc5646faa541d
khjsla <rkdguswlsla@naver.com>
2021-04-22 15:16:10 +0900

    add some chap2 code

---
+++ chap2_1/chap2_1.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("icodelab^^  ");
+  Serial.println("icodelab^^  ");
+  delay(1000);
+}
